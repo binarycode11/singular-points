a556d1b9c81b10756b3f8fa8289004b25e94367c
wagner luiz <rengaw.luiz@gmail.com>
2023-08-01 18:17:09 -0300

    limpando

--- old/teste3.py
+++
@@ -1,8 +0,0 @@
-import numpy
-import numpy as np
-a = numpy.arange(10)
-include_index = np.arange(4)
-mask = np.ones(len(a), dtype=bool) # all elements included/True.
-mask[include_index] = False              # Set unwanted elements to False
-
-print(a[mask],a[~mask])
